10cbb065f1c26de4f40020bc64e99b8afb091ac0
samuelalcaraz634 <stevansamuel2@gmail.com>
2025-09-18 11:54:38 -0500

    3 commit

---
+++ Bolera/ut_presentacion/Repositorios/Ventas .cs
@@ -0,0 +1,78 @@
+﻿using lib_dominio.Entidades;
+using lib_repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace lib_repositorios.Implementaciones
+{
+    public class VentasAplicacion : IEmpleados_VentasAplicacion
+    {
+        private IConexion? IConexion = null;
+
+        public Empleados_VentasAplicacion(IConexion iConexion)
+        {
+            this.IConexion = iConexion;
+        }
+
+        public void Configurar(string StringConexion)
+        {
+            this.IConexion!.StringConexion = StringConexion;
+        }
+
+        public Empleados_Ventas? Borrar(Empleados_Ventas? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Operaciones
+            entidad._Dueño = null;
+            entidad._Mascota = null;
+
+            this.IConexion!.Empleados_Ventas!.Remove(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public Empleados_Ventas? Guardar(Empleados_Ventas? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad.Id != 0)
+                throw new Exception("lbYaSeGuardo");
+
+            // Operaciones
+            entidad._Dueño = null;
+            entidad._Mascota = null;
+
+            this.IConexion!.Empleados_Ventas!.Add(entidad);
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+
+        public List<Empleados_Ventas> Listar()
+        {
+            return this.IConexion!.Empleados_Ventas!.Take(20).ToList();
+        }
+
+        public Empleados_Ventas? Modificar(Empleados_Ventas? entidad)
+        {
+            if (entidad == null)
+                throw new Exception("lbFaltaInformacion");
+
+            if (entidad!.Id == 0)
+                throw new Exception("lbNoSeGuardo");
+
+            // Operaciones
+            entidad._Dueño = null;
+            entidad._Mascota = null;
+
+            var entry = this.IConexion!.Entry<Empleados_Ventas>(entidad);
+            entry.State = EntityState.Modified;
+            this.IConexion.SaveChanges();
+            return entidad;
+        }
+    }
+}
